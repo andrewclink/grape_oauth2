5053feeab1f17be56b5fc71ca88cdd7cdae25ae4
Andrew Clink <andrew@elementsmi.com>
2017-01-16 15:52:23 -0700

    Forgot a comma

--- lib/grape_oauth2/generators/token.rb
+++ lib/grape_oauth2/generators/token.rb
@@ -8,7 +8,7 @@ module Grape
         STRATEGY_CLASSES = {
           password: Grape::OAuth2::Strategies::Password,
           client_credentials: Grape::OAuth2::Strategies::ClientCredentials,
-          refresh_token: Grape::OAuth2::Strategies::RefreshToken
+          refresh_token: Grape::OAuth2::Strategies::RefreshToken,
           authorization_code: Grape::Oauth2::Strategies::AuthorizationCode 
         }.freeze
 
