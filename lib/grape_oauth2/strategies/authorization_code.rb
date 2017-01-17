4fcdfa1579d6f56f8686056f5d456d8ba76ff95e
Andrew Clink <andrew@elementsmi.com>
2017-01-16 17:30:30 -0700

    remove req type detection

--- lib/grape_oauth2/strategies/authorization_code.rb
+++ lib/grape_oauth2/strategies/authorization_code.rb
@@ -15,33 +15,17 @@ module Grape
             # Request: #<Rack::OAuth2::Server::Token::AuthorizationCode::Request:0x00000003891f18>
             # Response: #<Rack::OAuth2::Server::Token::Response:0x00000003891ae0>
             
-            puts request.response_type
+            
 
             # TODO: verify scopes if they valid
             # scopes = request.scope
             # request.invalid_scope! "Unknown scope: #{scope}"
 
-            case request.response_type
-            when :code
-              raise "No, this code is not dead" # Please comment as to what caused this code to run
 
-              # Verify Redirect
-              response.redirect_uri = request.verify_redirect_uri!(client.redirect_uri)
-
-              # resource owner can't be nil!
-              authorization_code = config.access_grant_class.create_for(client, nil, response.redirect_uri)
-              response.code = authorization_code.token
-
-              response.approve!
-            when :token
-              
-              # Verify Redirect
-              
-              
-              # resource owner can't be nil!
-              access_token = config.access_token_class.create_for(client, nil, scopes_from(request))
-              response.access_token = expose_to_bearer_token(access_token)
-            end
+            
+            # resource owner can't be nil!
+            access_token = config.access_token_class.create_for(client, nil, scopes_from(request))
+            response.access_token = expose_to_bearer_token(access_token)
 
             response
           end
